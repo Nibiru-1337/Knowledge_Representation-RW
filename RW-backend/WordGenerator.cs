65c02a56d292abe2196ff851f0774b7e71cece27
Nibiru-1337 <3psilion@gmail.com>
2017-04-29 20:27:46 +0200

    Initial backend classes

---
+++ RW-backend/WordGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("RW-tests")]
+namespace RW_backend
+{
+    public static class WordGenerator
+    {
+        public static OrderedDictionary[] GenerateWorldNodes(OrderedDictionary fluents, List<RwAction> Actions)
+        {
+            // number of nodes in graph
+            uint TotalNodes = (uint)Math.Pow(2, fluents.Count);
+            // graph representation = table of string->bool dictionaries
+            OrderedDictionary[] Nodes = new OrderedDictionary[TotalNodes];
+            //set fluent values in nodes
+            for (uint node = 0; node < TotalNodes; node++)
+            {
+                Nodes[node] = new OrderedDictionary();
+                for (int keyIdx = 0; keyIdx < fluents.Count; keyIdx++)
+                {
+                    var item = fluents.Cast<DictionaryEntry>().ElementAt(keyIdx);
+                    var bit = (node & (1 << keyIdx)) != 0;
+                    Nodes[node].Add(item.Key, bit);
+                }
+            }
+            return Nodes;
+        }
+
+        static void Main(string[] args){}
+
+    }
+}
---
+++ RW-tests/TestsBackend.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RW_backend;
+
+namespace RW_tests
+{
+    [TestClass]
+    public class TestsBackend
+    {
+        [TestMethod]
+        public void Simple_Yale_Shooting_Problem_Test()
+        {
+            OrderedDictionary fluents = new OrderedDictionary();
+            //initial fluents
+            fluents.Add("loaded", false);
+            fluents.Add("alive", true);
+
+            //create LOAD action
+            //LOAD has no conditions for execution
+            Dictionary<string, bool> conditions = new Dictionary<string, bool>();
+            //LOAD execution causes loaded
+            Dictionary<string, bool> results = new Dictionary<string, bool>();
+            results.Add("loaded", true);
+            RwAction LOAD = new RwAction(conditions, results);
+
+            //create SHOOT action
+            //SHOOT has conditions for execution
+            conditions = new Dictionary<string, bool>();
+            conditions.Add("loaded", true);
+            //SHOOT execution causes loaded
+            results = new Dictionary< string, bool> ();
+            results.Add("loaded", false);
+            results.Add("alive", false);
+            RwAction SHOOT = new RwAction(conditions, results);
+
+            List<RwAction> Actions = new List<RwAction>(2);
+            Actions.Add(SHOOT);
+            Actions.Add(LOAD);
+
+            OrderedDictionary[] nodes = WordGenerator.GenerateWorldNodes(fluents, Actions);
+        }
+    }
+}
