b79701e50ca099696a919d1053f3b08727001beb
Pico11 <jastrzebskip@student.mini.pw.edu.pl>
2017-05-03 15:11:23 +0200

    checkpoint goal test

--- RW-backend/BackendLogic.cs
+++ RW-backend/BackendLogic.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RW_backend
 {
@@ -12,5 +8,9 @@ namespace RW_backend
     public class BackendLogic
     {
         //TODO budowanie reprezentaji świata i odpowiadanie na kwerendy
+        public World CalculateWorld(Model model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
