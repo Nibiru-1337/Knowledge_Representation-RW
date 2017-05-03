32222dbe0c48e6f030ef3e96f2421f03c4cea6f5
Pico11 <jastrzebskip@student.mini.pw.edu.pl>
2017-05-03 14:30:21 +0200

    checkpoint goal written in unit test - stub
    this test should pass before checkpoint - meaning that we have input parsing, simple formula evaluation and simple query execution
    
    added method for automated creation of VM (someone implementing vm may hate me for this) which helps with tests
    
    based on VM Model should be created
    later world graph
    and finally simple query evaluation should be possible

--- RW-Frontend/VM.cs
+++ RW-Frontend/VM.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using RW_Frontend.Annotations;
 
+[assembly: InternalsVisibleTo("RW-tests")]
 namespace RW_Frontend
 {
     class VM : INotifyPropertyChanged
@@ -11,12 +14,12 @@ namespace RW_Frontend
         public ObservableCollection<string> Fluents { get; set; }
         public ObservableCollection<string> Actions { get; set; }
         public ObservableCollection<string> Agents { get; set; }
-        
+
         public ObservableCollection<string> Noninertial { get; set; }
-        
+
         public ObservableCollection<string> AlwaysStatements { get; set; }
         public ObservableCollection<string> InitiallyStatements { get; set; }
-        
+
         public ObservableCollection<string> AfterStatements { get; set; }
         public ObservableCollection<string> CausesStatements { get; set; }
         public ObservableCollection<string> ReleasesStatements { get; set; }
@@ -37,6 +40,26 @@ namespace RW_Frontend
         public static VM Create()
         {
             return new VM();
+        }
+
+        internal static VM Create(string[] fluents, string[] actions, string[] agents,
+            string[] noninertial, string[] always, string[] initially,
+            string[] after, string[] causes, string[] releases)
+        {
+            //metoda na potrzeby automatyzacji testów
+            return new VM
+            {
+                //trzeba zaktualizować przy zmianie reprezentacji zdań 
+                Fluents = new ObservableCollection<string>(fluents),
+                Actions = new ObservableCollection<string>(actions),
+                Agents = new ObservableCollection<string>(agents),
+                Noninertial = new ObservableCollection<string>(noninertial),
+                AlwaysStatements = new ObservableCollection<string>(always),
+                InitiallyStatements = new ObservableCollection<string>(initially),
+                AfterStatements = new ObservableCollection<string>(after),
+                CausesStatements = new ObservableCollection<string>(causes),
+                ReleasesStatements = new ObservableCollection<string>(releases)
+            };
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
