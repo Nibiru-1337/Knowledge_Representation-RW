8ca25326f6a305b06525ca47bfd56a3e7345ad8d
tymon <tamtamy.tymona@gmail.com>
2017-05-03 22:03:12 +0200

    [tests] simple logic clauses tested and working

---
+++ RW-tests/LogicTests/Utilities.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RW_backend.Models;
+using RW_backend.Models.Clauses.LogicClauses;
+
+namespace RW_tests.LogicTests
+{
+	class Utilities
+	{
+		public void SetFluents(List<int> fluents, bool negated, SimpleLogicClause clause)
+		{
+			foreach (int fluent in fluents)
+			{
+				clause.AddFluent(fluent, negated);
+			}
+		}
+
+		public State GetState(List<int> fs)
+		{
+			int state = 0;
+			BitValueOperator bop = new BitValueOperator();
+			foreach (int f in fs)
+			{
+				state = bop.SetFluent(state, f);
+			}
+			return new State(state);
+		}
+
+		public void WriteOutBitValue(int value)
+		{
+			BitValueOperator bop = new BitValueOperator();
+			for (int i = 0; i < sizeof(int) * 8; i++)
+			{
+				Console.Write((bop.GetValue(value, i) ? "1" : "0"));
+			}
+			Console.WriteLine(" = " + value);
+		}
+
+
+
+		public class Pair
+		{
+			public int Fluent;
+			public bool Negated;
+
+			public Pair(int f, bool n)
+			{
+				Fluent = f;
+				Negated = n;
+			}
+		}
+
+	}
+}
