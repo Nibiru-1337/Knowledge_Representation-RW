8510410cdf7f9f230fc0b1f2eba6eb1f74981423
Pico11 <jastrzebskip@student.mini.pw.edu.pl>
2017-05-03 14:30:21 +0200

    checkpoint goal written in unit test - stub
    this test should pass before checkpoint - meaning that we have input parsing, simple formula evaluation and simple query execution
    
    added method for automated creation of VM (someone implementing vm may hate me for this) which helps with tests
    
    based on VM Model should be created
    later world graph
    and finally simple query evaluation should be possible

---
+++ RW-tests/GoalFor_10_05_Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RW_backend;
+using RW_Frontend;
+
+namespace RW_tests
+{
+    [TestClass]
+    public class GoalFor1005Test
+    {
+        [TestMethod]
+        public void YaleScenarioTest()
+        {
+            var model = PrepareModel();
+            var logic = new BackendLogic();
+            ////dalej jak już będziemy mieć kwerendy, to coś w rodzaju
+            //var world = logic.CalculateWorld();
+            //var query = Query.Create("!alive after SHOOT");
+            //var queryResult = query.Evaluate(world);
+            //Assert.IsTrue(queryResult.IsTrue);
+            //Assert.IsNotNull(queryResult.Function);
+        }
+
+        private static Model PrepareModel()
+        {
+            var vm = VM.Create(noninertial: new string[0],
+                fluents: new[] { "loaded", "alive" },
+                actions: new[] { "LOAD", "SHOOT" },
+                agents: new[] { "Bob" },
+                always: new string[0],
+                initially: new[] { "loaded", "alive" },
+                after: new string[0],
+                causes: new[] { "SHOOT by {Bob} causes !alive if loaded" },
+                releases: new string[0]
+            );
+
+            var converter = new ModelConverter();
+            var model = converter.ConvertToModel(vm);
+            return model;
+        }
+    }
+}
