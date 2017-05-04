c9c54adfeec3caac7407cbcdd2cbe128c6dfe7c4
Nibiru-1337 <3psilion@gmail.com>
2017-05-04 16:51:08 +0200

    started work on World representaion (work-in-progess)

--- RW-tests/SceneriosTests/TestsBackend.cs
+++ RW-tests/SceneriosTests/TestsBackend.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Specialized;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RW_backend.Models;
+using RW_backend.Models.Clauses;
+using RW_backend.Models.Clauses.LogicClauses;
 
 namespace RW_tests.SceneriosTests
 {
@@ -8,44 +11,17 @@ namespace RW_tests.SceneriosTests
     public class TestsBackend
     {
         [TestMethod]
-        public void Simple_Yale_Shooting_Problem_Test()
+        public void ExampleWorldTest()
         {
-			//TODO: adjust
-            OrderedDictionary fluents = new OrderedDictionary();
-            //initial fluents
-            fluents.Add("loaded", false);
-            fluents.Add("alive", true);
-
-            //create LOAD action
-            //LOAD has no conditions for execution
-            Dictionary<string, bool> conditions = new Dictionary<string, bool>();
-            //LOAD execution causes loaded
-            Dictionary<string, bool> results = new Dictionary<string, bool>();
-            results.Add("loaded", true);
-            //RwAction LOAD = new RwAction(conditions, results);
-
-            //create SHOOT action
-            //SHOOT has conditions for execution
-            conditions = new Dictionary<string, bool>();
-            conditions.Add("loaded", true);
-            //SHOOT execution causes loaded
-            results = new Dictionary< string, bool> ();
-            results.Add("loaded", false);
-            results.Add("alive", false);
-           // RwAction SHOOT = new RwAction(conditions, results);
-
-            //List<RwAction> Actions = new List<RwAction>(2);
-           // Actions.Add(SHOOT);
-            //Actions.Add(LOAD);
-
-           // List<OrderedDictionary> nodes = WorldOperations.GenerateWorldNodes(fluents);
-            //Assert.AreEqual(nodes.Count, 4);
-            //List<OrderedDictionary> ConnectedStates = WorldOperations.Resolution(LOAD, fluents, nodes);
-            //Assert.AreEqual(ConnectedStates[0]["alive"], true);
-            //Assert.AreEqual(ConnectedStates[0]["loaded"], true);
-            //ConnectedStates = WorldOperations.Resolution(SHOOT, ConnectedStates[0], nodes);
-            //Assert.AreEqual(ConnectedStates[0]["alive"], false);
-            //Assert.AreEqual(ConnectedStates[0]["loaded"], false);
+            //world with 2 fluents ex. alive=0, loaded=1
+            World world = new World(2);
+            AlternativeOfConjunctions effect = new AlternativeOfConjunctions();
+            Conjunction c = new Conjunction();
+            //loaded is set after action
+            c.AddFluent(1,false);
+            effect.AddConjunction(c);
+            Causes a = new Causes(null,effect,1,0);
+            world.AddCauses(a);
         }
     }
 }
