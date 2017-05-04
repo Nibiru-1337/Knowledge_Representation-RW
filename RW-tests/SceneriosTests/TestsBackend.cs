f706cbf9387c2637a77fad81845b097cda7fd8eb
Nibiru-1337 <3psilion@gmail.com>
2017-05-05 00:12:34 +0200

    Fixed and improved World representation tests

--- RW-tests/SceneriosTests/TestsBackend.cs
+++ RW-tests/SceneriosTests/TestsBackend.cs
@@ -16,17 +16,25 @@ namespace RW_tests.SceneriosTests
         {
             //world with 2 fluents ex. alive=0, loaded=1
             World world = new World(2);
-            Alternative effect = new Alternative();
+            
             //loaded is set after LOAD
-            effect.AddFluent(1,false);
-            Causes LOAD = new Causes(null,effect,0x1,0x0);
+            Alternative effect = new Alternative();
+            effect.AddFluent(1, false);
+            Causes LOAD = new Causes(null, effect, 0x1, 0x0);
             world.AddCauses(LOAD);
 
             //should have an edge from state alive, -loaded -> alive, loaded
             List<State> afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x1))];
             Assert.AreEqual(1, afterLOAD.Count);
             Assert.AreEqual(new State(0x3), afterLOAD[0]);
-
+            //should have an edge from state alive, loaded -> alive, loaded
+            afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x3))];
+            Assert.AreEqual(1, afterLOAD.Count);
+            Assert.AreEqual(new State(0x3), afterLOAD[0]);
+            //should have an edge from state -alive, loaded -> -alive, loaded
+            afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x2))];
+            Assert.AreEqual(1, afterLOAD.Count);
+            Assert.AreEqual(new State(0x2), afterLOAD[0]);
             //should have an edge from state -alive, -loaded -> -alive, loaded
             afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x0))];
             Assert.AreEqual(1, afterLOAD.Count);
@@ -35,16 +43,34 @@ namespace RW_tests.SceneriosTests
             //-alive after SHOOT with loaded condition by agent with index = 0 (bob or whatever)
             Alternative conditions = new Alternative();
             conditions.AddFluent(1, false);
-            effect = new Alternative();
-            effect.AddFluent(1, true);
-            Causes SHOOT = new Causes(conditions, effect, 0x2, 0x1);
+            Conjunction effect2 = new Conjunction();
+            effect2.AddFluent(0, true);
+            effect2.AddFluent(1, true);
+            Causes SHOOT = new Causes(conditions, effect2, 0x2, 0x1);
             world.AddCauses(SHOOT);
 
             //should have an edge from state alive, loaded -> -alive, -loaded
-            List<State> afterSHOOT = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x1))];
+            List<State> afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x3))];
             Assert.AreEqual(1, afterLOAD.Count);
-            Assert.AreEqual(new State(0x3), afterSHOOT[0]);
+            Assert.AreEqual(new State(0x0), afterSHOOT[0]);
+            //should have an edge from state -alive, loaded -> -alive, -loaded
+            afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x2))];
+            Assert.AreEqual(1, afterLOAD.Count);
+            Assert.AreEqual(new State(0x0), afterSHOOT[0]);
+            //should not have edges from state alive, -loaded
+            try
+            {
+                afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x1))];
+                Assert.Fail();
+            } catch (KeyNotFoundException) { }
 
+            //should not have edges from alive, -loaded
+            try
+            {
+                afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x0))];
+                Assert.Fail();
+            } catch(KeyNotFoundException) { }
+            
         }
     }
 }
