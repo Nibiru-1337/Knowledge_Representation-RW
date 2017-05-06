aad87633a65da6f7c4f6c23c898113b829e9c343
deus-ex-silicium <9478653+deus-ex-silicium@users.noreply.github.com>
2017-05-06 14:10:10 +0200

    [world] changed graph edges representation, edited world test, and some refactoring

---
+++ RW-backend/Models/AgentSetChecker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using RW_backend.Models.BitSets;
+using RW_backend.Models.Clauses;
+
+namespace RW_backend.Models
+{
+    public class AgentSetChecker
+    {
+        private int _agentSet;
+        public List<State> edges { get; private set; }
+        public AgentSetChecker(int agentSet, List<State> x)
+        {
+            _agentSet = agentSet;
+            edges = x;
+        }
+
+        /// <summary>
+        /// Checks if the agent set is allowed to execute this action
+        /// </summary>
+        /// <param name="agentSet">The set executing the action</param>
+        /// <returns>true if input is subset of action agent set, false otherwise</returns>
+        public bool Check(int agentSet)
+        {
+            if ((_agentSet & agentSet) != 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}
--- RW-tests/SceneriosTests/TestsBackend.cs
+++ RW-tests/SceneriosTests/TestsBackend.cs
@@ -13,34 +13,19 @@ namespace RW_tests.SceneriosTests
     [TestClass]
     public class TestsBackend
     {
-        [TestMethod]
-        public void ExampleWorldTest()
+        private World _SetUpWorld()
         {
-            //world with 2 fluents ex. alive=0, loaded=1
+            //world with 2 fluents ex. alive=0, loaded=1 sand 1 agent Bob=0
             World world = new World(2);
-            
-            //loaded is set after LOAD
+
+            //add 2 actions, LOAD and SHOOT executed by Bob
+            //LOADID=0, SHOOTID=1
+
+            //loaded is set after LOAD by agent with index = 0 (bob or whatever)
             UniformAlternative effect = new UniformAlternative();
             effect.AddFluent(1, false);
-            Causes LOAD = new Causes(null, effect, 0x1, new AgentsSet(0x0));
+            Causes LOAD = new Causes(null, effect, 0x0, new AgentsSet(0x1));
             world.AddCauses(LOAD);
-
-            //should have an edge from state alive, -loaded -> alive, loaded
-            List<State> afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x1))];
-            Assert.AreEqual(1, afterLOAD.Count);
-            Assert.AreEqual(new State(0x3), afterLOAD[0]);
-            //should have an edge from state alive, loaded -> alive, loaded
-            afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x3))];
-            Assert.AreEqual(1, afterLOAD.Count);
-            Assert.AreEqual(new State(0x3), afterLOAD[0]);
-            //should have an edge from state -alive, loaded -> -alive, loaded
-            afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x2))];
-            Assert.AreEqual(1, afterLOAD.Count);
-            Assert.AreEqual(new State(0x2), afterLOAD[0]);
-            //should have an edge from state -alive, -loaded -> -alive, loaded
-            afterLOAD = world.Connections[new Tuple<Causes, State>(LOAD, new State(0x0))];
-            Assert.AreEqual(1, afterLOAD.Count);
-            Assert.AreEqual(new State(0x2), afterLOAD[0]);
 
             //-alive after SHOOT with loaded condition by agent with index = 0 (bob or whatever)
             UniformAlternative conditions = new UniformAlternative();
@@ -48,28 +33,82 @@ namespace RW_tests.SceneriosTests
             UniformConjunction effect2 = new UniformConjunction();
             effect2.AddFluent(0, true);
             effect2.AddFluent(1, true);
-            Causes SHOOT = new Causes(conditions, effect2, 0x2, new AgentsSet(0x1));
+            Causes SHOOT = new Causes(conditions, effect2, 0x1, new AgentsSet(0x1));
             world.AddCauses(SHOOT);
 
-            //should have an edge from state alive, loaded -> -alive, -loaded
-            List<State> afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x3))];
-            Assert.AreEqual(1, afterLOAD.Count);
-            Assert.AreEqual(new State(0x0), afterSHOOT[0]);
-            //should have an edge from state -alive, loaded -> -alive, -loaded
-            afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x2))];
-            Assert.AreEqual(1, afterLOAD.Count);
-            Assert.AreEqual(new State(0x0), afterSHOOT[0]);
-            //should not have edges from state alive, -loaded
+            return world;
+        }
+
+        [TestMethod]
+        public void WorldWithLoadedAliveAndBob()
+        {
+            //LOAD is actionId = 0 , SHOOT is actionId = 1
+            World world = _SetUpWorld();
+
+            //(LOAD) should have an edge from state alive, -loaded -> alive, loaded
+            AgentSetChecker asc = world.Connections[new Tuple<int, State>(world.ActionIds[0], new State(0x1))];
+            if (asc.Check(0x0)) Assert.Fail(); //empty agent set should not execute LOAD
+            if (asc.Check(0x1))
+            {
+                List<State> afterLOAD = asc.edges;
+                Assert.AreEqual(1, afterLOAD.Count);
+                Assert.AreEqual(new State(0x3), afterLOAD[0]);
+            }
+            //(LOAD) should have an edge from state alive, loaded -> alive, loaded
+            asc = world.Connections[new Tuple<int, State>(world.ActionIds[0], new State(0x3))];
+            if (asc.Check(0x2)) Assert.Fail(); //Bob is not present, not executable
+            if (asc.Check(0x1))
+            {
+                List<State> afterLOAD = asc.edges;
+                Assert.AreEqual(1, afterLOAD.Count);
+                Assert.AreEqual(new State(0x3), afterLOAD[0]);
+            }
+            //(LOAD) should have an edge from state -alive, loaded -> -alive, loaded
+            asc = world.Connections[new Tuple<int, State>(world.ActionIds[0], new State(0x2))];
+            if (asc.Check(0x3)) //Bob is present, executable
+            {
+                List<State> afterLOAD = asc.edges;
+                Assert.AreEqual(1, afterLOAD.Count);
+                Assert.AreEqual(new State(0x2), afterLOAD[0]);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+            //(LOAD) should have an edge from state -alive, -loaded -> -alive, loaded
+            asc = world.Connections[new Tuple<int, State>(world.ActionIds[0], new State(0x0))];
+            if (asc.Check(0x1)) {
+                List<State> afterLOAD = asc.edges;
+                Assert.AreEqual(1, afterLOAD.Count);
+                Assert.AreEqual(new State(0x2), afterLOAD[0]);
+            }
+            //(SHOOT) should have an edge from state alive, loaded -> -alive, -loaded
+            asc = world.Connections[new Tuple<int, State>(world.ActionIds[1], new State(0x3))];
+            if (asc.Check(0x1))
+            {
+                List<State> afterSHOOT = asc.edges;
+                Assert.AreEqual(1, afterSHOOT.Count);
+                Assert.AreEqual(new State(0x0), afterSHOOT[0]);
+            }
+            //(SHOOT) should have an edge from state -alive, loaded -> -alive, -loaded
+            asc = world.Connections[new Tuple<int, State>(world.ActionIds[1], new State(0x2))];
+            if (asc.Check(0x1))
+            {
+                List<State> afterSHOOT = asc.edges;
+                Assert.AreEqual(1, afterSHOOT.Count);
+                Assert.AreEqual(new State(0x0), afterSHOOT[0]);
+            }
+            //(SHOOT) should not have edges from state alive, -loaded
             try
             {
-                afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x1))];
+                asc = world.Connections[new Tuple<int, State>(world.ActionIds[1], new State(0x1))];
                 Assert.Fail();
             } catch (KeyNotFoundException) { }
 
-            //should not have edges from alive, -loaded
+            //(SHOOT) should not have edges from alive, -loaded
             try
             {
-                afterSHOOT = world.Connections[new Tuple<Causes, State>(SHOOT, new State(0x0))];
+                asc = world.Connections[new Tuple<int, State>(world.ActionIds[1], new State(0x0))];
                 Assert.Fail();
             } catch(KeyNotFoundException) { }
             
