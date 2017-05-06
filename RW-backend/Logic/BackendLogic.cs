6599f1b257aac354623e7ef028cb6d8dc720d54f
tymon <tamtamy.tymona@gmail.com>
2017-05-07 00:52:06 +0200

    [refactor] small refactor + opis rzeczy do zrobienia w BackendLogic w tworzeniu świata

--- RW-backend/Logic/BackendLogic.cs
+++ RW-backend/Logic/BackendLogic.cs
@@ -8,15 +8,22 @@ namespace RW_backend.Logic
     /// </summary>
     public class BackendLogic
     {
-		WorldConstructor WorldConstructor = new WorldConstructor();
+		
 
 
         //TODO budowanie reprezentaji świata i odpowiadanie na kwerendy
         public World CalculateWorld(Model model)
         {
-	        return WorldConstructor.CalculateWorld(model);
+			// 1. tworzenie listy wszystkich możliwych stanów (uwzględniając wszystkie always)
+			// 2. tworzenie krawędzi na podstawie Causes i Releases
+			//		dla każdej akcji znajdujemy wszystkie możliwe causes i releases
+			//		dla każdego zdania causes znajdujemy wszystkie możliwe przejścia do stanów
+			//		minimalizujemy zbiory na podstawie tego co otrzymaliśmy i zdań releases + fluentów nieinertnych
+			//		(UWAGA: podczas rezolucji chyba też będziemy musieli minimalizować? nie wiem...)
+			//	3. zapisujemy gdzieś After	
+
+
+	        throw new NotImplementedException();
         }
-
-
     }
 }
