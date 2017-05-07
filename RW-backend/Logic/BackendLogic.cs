e78324675562293804e9af59e02d13f9a201e456
Nibiru-1337 <3psilion@gmail.com>
2017-05-07 15:43:12 +0200

    [world] changed how edges are represented, further world development and refactoring, added support for Always statements during World construction

--- RW-backend/Logic/BackendLogic.cs
+++ RW-backend/Logic/BackendLogic.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using RW_backend.Models;
+using RW_backend.Models.BitSets;
+using RW_backend.Models.Clauses;
+using RW_backend.Models.Clauses.LogicClauses;
 
 namespace RW_backend.Logic
 {
@@ -14,16 +17,19 @@ namespace RW_backend.Logic
         //TODO budowanie reprezentaji świata i odpowiadanie na kwerendy
         public World CalculateWorld(Model model)
         {
-			// 1. tworzenie listy wszystkich możliwych stanów (uwzględniając wszystkie always)
-			// 2. tworzenie krawędzi na podstawie Causes i Releases
-			//		dla każdej akcji znajdujemy wszystkie możliwe causes i releases
-			//		dla każdego zdania causes znajdujemy wszystkie możliwe przejścia do stanów
-			//		minimalizujemy zbiory na podstawie tego co otrzymaliśmy i zdań releases + fluentów nieinertnych
-			//		(UWAGA: podczas rezolucji chyba też będziemy musieli minimalizować? nie wiem...)
-			//	3. zapisujemy gdzieś After	
+            // 1. tworzenie listy wszystkich możliwych stanów (uwzględniając wszystkie always)
+            // 2. tworzenie krawędzi na podstawie Causes i Releases
+            //		dla każdej akcji znajdujemy wszystkie możliwe causes i releases
+            //		dla każdego zdania causes znajdujemy wszystkie możliwe przejścia do stanów
+            //		minimalizujemy zbiory na podstawie tego co otrzymaliśmy i zdań releases + fluentów nieinertnych
+            //		(UWAGA: podczas rezolucji chyba też będziemy musieli minimalizować? nie wiem...)
+            //	3. zapisujemy gdzieś After	
 
+            World world = new World(model.FluentsCount, model.AlwaysStatements);
 
-	        throw new NotImplementedException();
+            world.AddCauses(model.CausesStatements, model.ActionsCount);
+
+            return world;
         }
     }
 }
