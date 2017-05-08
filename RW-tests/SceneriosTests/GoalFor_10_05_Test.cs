7c4f6c8f16d86253124575f3c57accd41dfa322d
Pico11 <jastrzebskip@student.mini.pw.edu.pl>
2017-05-08 13:54:41 +0200

    GoalFor_10_05_Test scenario descriptions, extracted preparation code

--- RW-tests/SceneriosTests/GoalFor_10_05_Test.cs
+++ RW-tests/SceneriosTests/GoalFor_10_05_Test.cs
@@ -5,6 +5,7 @@ using RW_backend.Logic;
 using RW_backend.Logic.Queries;
 using RW_backend.Models;
 using RW_backend.Models.BitSets;
+using RW_backend.Models.World;
 using RW_Frontend;
 using RW_Frontend.InputsViewModels;
 
@@ -13,14 +14,23 @@ namespace RW_tests.SceneriosTests
     [TestClass]
     public class GoalFor1005Test
     {
+        private static World PrepareWorld(VM vm)
+        {
+            var model = PrepareModel(vm);
+            var logic = new BackendLogic();
+            var world = logic.CalculateWorld(model);
+            return world;
+        }
+
+        /// <summary>
+        /// Sprawdź wykonanie YSP ze stanu (alive,loaded) po akcji SHOOT do (!alive,!loaded)
+        /// </summary>
         [TestMethod]
         public void YaleScenarioTest()
         {
             var vm = PrepareVM();
-            var model = PrepareModel(vm);
-            var logic = new BackendLogic();
-            var world = logic.CalculateWorld(model);
-            var query = PrepareAfterQuery_Shoot(vm, false, "loaded");
+            var world = PrepareWorld(vm);
+            var query = PrepareAfterQuery_Shoot(vm, false, "alive&loaded");
             var queryResult = query.Evaluate(world);
             Assert.IsTrue(queryResult.IsTrue, "query should be true");
             Assert.IsNotNull(queryResult.Function, "function should not be null");
@@ -28,38 +38,42 @@ namespace RW_tests.SceneriosTests
             Assert.AreEqual(new State(0), queryResult.Function[1]);//!alive,!loaded
         }
 
+        /// <summary>
+        /// Sprawdź wartość kwerendy possibly !alive after SHOOT by Bob from (alive,loaded)
+        /// </summary>
         [TestMethod]
         public void YaleScenarioSuccesOnlyTest_ShootLoaded()
         {
             var vm = PrepareVM();
-            var model = PrepareModel(vm);
-            var logic = new BackendLogic();
-            var world = logic.CalculateWorld(model);
-            var query = PrepareAfterQuery_Shoot(vm, false, "loaded");
+            var world = PrepareWorld(vm);
+            var query = PrepareAfterQuery_Shoot(vm, false, "alive&loaded");
             var queryResult = query.Evaluate(world);
             Assert.IsTrue(queryResult.IsTrue, "query should be true - SHOOT with loaded");
         }
 
+        /// <summary>
+        /// Sprawdź wartość kwerendy possibly !alive after SHOOT by Bob from (alive,!loaded)
+        /// </summary>
         [TestMethod]
         public void YaleScenarioSuccesOnlyTest_ShootNotLoaded()
         {
             var vm = PrepareVM();
-            var model = PrepareModel(vm);
-            var logic = new BackendLogic();
-            var world = logic.CalculateWorld(model);
-            var query = PrepareAfterQuery_Shoot(vm, false, "!loaded");
+            var world = PrepareWorld(vm);
+            var query = PrepareAfterQuery_Shoot(vm, false, "alive&!loaded");
             var queryResult = query.Evaluate(world);
             Assert.IsFalse(queryResult.IsTrue, "query should not be true - SHOOT with !loaded");
         }
 
+
+        /// <summary>
+        /// Sprawdź wartość kwerend possibly/necessary !alive after SHOOT by Bob from (alive)
+        /// </summary>
         [TestMethod]
         public void YaleScenarioSuccesOnlyTest_Shoot()
         {
             var vm = PrepareVM();
-            var model = PrepareModel(vm);
-            var logic = new BackendLogic();
-            var world = logic.CalculateWorld(model);
-            var query = PrepareAfterQuery_Shoot(vm, false, "");
+            var world = PrepareWorld(vm);
+            var query = PrepareAfterQuery_Shoot(vm, false, "alive");
             var queryResult = query.Evaluate(world);
             Assert.IsTrue(queryResult.IsTrue, "possibly query should be true");
             query = PrepareAfterQuery_Shoot(vm, true, "");
@@ -74,7 +88,7 @@ namespace RW_tests.SceneriosTests
             var agentsViewModels = vm.Agents.Select(agent => new AgentViewModel(agent)).ToList();
             return new ModelConverter().ConvertAfterQuery(CreateAfterQueryVM_Shoot(always, initial), agentsViewModels, actionsViewModels, fluentsViewModels);
         }
-        
+
         private static AfterQueryViewModel CreateAfterQueryVM_Shoot(bool always, string initial)
         {
             var after = new AfterQueryViewModel(always ? AfterQueryViewModel.AfterQueryNecOrPos.Necessary : AfterQueryViewModel.AfterQueryNecOrPos.Possibly,
@@ -88,7 +102,7 @@ namespace RW_tests.SceneriosTests
             var actions = vm.Actions.Select(action => new ActionViewModel(action)).ToList();
             var agents = vm.Agents.Select(agent => new AgentViewModel(agent)).ToList();
 
-            var causes = new List<CausesClauseViewModel> { new CausesClauseViewModel("SHOOT", new List<string> { "Bob" }, "!alive", "loaded") };
+            var causes = new List<CausesClauseViewModel> { new CausesClauseViewModel("SHOOT", new List<string> { "Bob" }, "!alive", "loaded"), new CausesClauseViewModel("SHOOT", new List<string> { "Bob" }, "!loaded", "") };
 
             var converter = new ModelConverter();
             var model = converter.ConvertToModel(fluents, actions, agents, causes);
