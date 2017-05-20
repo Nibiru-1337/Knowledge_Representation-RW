22d97103509bae99b12b2d9a626b7e67c699f8b5
patrykm <mundzpat@gmail.com>
2017-05-20 10:35:26 +0200

    [frontend] View models: always

---
+++ RW-Frontend/InputsViewModels/AlwaysClauseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace RW_Frontend.InputsViewModels
+{
+    public class AlwaysClauseViewModel
+    {
+        public string AlfaLogicExp { get; set; }
+
+        public AlwaysClauseViewModel(string alfaLogicexp)
+        {
+            this.AlfaLogicExp = alfaLogicexp;
+        }
+
+        public static string GetAlfaLogicExpFromView(StackPanel alwaysClauseStackPanel)
+        {
+            var alfaLogicExpTextBox = alwaysClauseStackPanel.Children[1] as TextBox;
+            if (alfaLogicExpTextBox == null)
+                return null;
+            return alfaLogicExpTextBox.Text;
+        }
+    }
+}
