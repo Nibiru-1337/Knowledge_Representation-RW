253206ce6928dcbf5d562e6209cc024d4652a305
Patiku <patiku@interia.eu>
2017-05-26 12:51:09 +0200

    First scenario test

---
+++ RW-tests/UltimateSystemTests/NonintertialFluents/NonIntertialTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RW_backend.Logic;
+using RW_backend.Logic.Queries;
+using RW_backend.Models;
+using RW_backend.Models.BitSets;
+using RW_backend.Models.Clauses.LogicClauses;
+using RW_backend.Models.Factories;
+using RW_backend.Models.World;
+
+namespace RW_tests.UltimateSystemTests.NonintertialFluents
+{
+    [TestClass]
+    public class NonIntertialTests
+    {
+        [TestMethod]
+        public void CheckForInertial()
+        {
+            Model model = BaseWorldGenerator.GenerateWorld(false);
+            World world = new BackendLogic().CalculateWorld(model);
+
+            LogicClausesFactory logicClausesFactory = new LogicClausesFactory();
+            AfterQuery query = new AfterQuery(new ActionAgentsPair[]
+            {
+                new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom),
+            }, 
+            new UniformAlternative(), true,
+                logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.BobRaised, FluentSign.Positive));
+            Assert.AreEqual(false, query.Evaluate(world).IsTrue);
+
+        }
+    }
+}
