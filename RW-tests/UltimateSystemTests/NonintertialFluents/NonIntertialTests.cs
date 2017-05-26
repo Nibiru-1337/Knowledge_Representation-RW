4e374abc051147c0336aa64484edff8b33e4ba45
Patiku <patiku@interia.eu>
2017-05-26 16:48:46 +0200

    CheckForNoninertialNoReleaseNoOrOneConditionFrom

--- RW-tests/UltimateSystemTests/NonintertialFluents/NonIntertialTests.cs
+++ RW-tests/UltimateSystemTests/NonintertialFluents/NonIntertialTests.cs
@@ -19,65 +19,72 @@ namespace RW_tests.UltimateSystemTests.NonintertialFluents
         {
             Model model = BaseWorldGenerator.GenerateWorld(false);
             World world = new BackendLogic().CalculateWorld(model);
-
+            ActionAgentsPair[] program = new ActionAgentsPair[] { new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom)};
             //always BobRaised after MOVE by Tom
             LogicClausesFactory logicClausesFactory = new LogicClausesFactory();
-            AfterQuery query = new AfterQuery(new ActionAgentsPair[]
-            {
-                new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom),
-            }, 
-            new UniformAlternative(), true,
+            AfterQuery query = new AfterQuery(program, new UniformAlternative(), true,
                 logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.BobRaised, FluentSign.Positive));
             Assert.AreEqual(false, query.Evaluate(world).IsTrue);
 
             //possibly BobRaised after MOVE by Tom
             logicClausesFactory = new LogicClausesFactory();
-            query = new AfterQuery(new ActionAgentsPair[]
-            {
-                new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom),
-            },
-            new UniformAlternative(), false,
+            query = new AfterQuery(program, new UniformAlternative(), false,
                 logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.BobRaised, FluentSign.Positive));
             Assert.AreEqual(true, query.Evaluate(world).IsTrue);
 
 
             //possibly TomRaised after MOVE by Tom
             logicClausesFactory = new LogicClausesFactory();
-            query = new AfterQuery(new ActionAgentsPair[]
-            {
-                new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom),
-            },
-            new UniformAlternative(), false,
+            query = new AfterQuery(program, new UniformAlternative(), false,
                 logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.TomRaised, FluentSign.Positive));
             Assert.AreEqual(true, query.Evaluate(world).IsTrue);
 
             //always TomRaised after MOVE by Tom
             logicClausesFactory = new LogicClausesFactory();
-            query = new AfterQuery(new ActionAgentsPair[]
-            {
-                new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom),
-            },
-            new UniformAlternative(), true,
+            query = new AfterQuery(program, new UniformAlternative(), true,
                 logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.TomRaised, FluentSign.Positive));
             Assert.AreEqual(false, query.Evaluate(world).IsTrue);
 
             //possibly TomRaised after MOVE by Tom from ~TomRaised
             logicClausesFactory = new LogicClausesFactory();
-            query = new AfterQuery(new ActionAgentsPair[]
-            {
-                new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom),
-            },
-            UniformAlternative.CreateFrom(new List<int>(), new List<int>() { ScenarioConsts.TomRaised }), false,
+            query = new AfterQuery(program, UniformAlternative.CreateFrom(new List<int>(), new List<int>() { ScenarioConsts.TomRaised }), false,
                 logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.TomRaised, FluentSign.Positive));
             Assert.AreEqual(true, query.Evaluate(world).IsTrue);
 
             //always TomRaised after MOVE by Tom from ~TomRaised
             logicClausesFactory = new LogicClausesFactory();
-            query = new AfterQuery(new ActionAgentsPair[]
-            {
-                new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom),
-            },
-            UniformAlternative.CreateFrom(new List<int>(), new List<int>() { ScenarioConsts.TomRaised }), true,
+            query = new AfterQuery(program, UniformAlternative.CreateFrom(new List<int>(), new List<int>() { ScenarioConsts.TomRaised }), true,
+                logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.TomRaised, FluentSign.Positive));
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue);
+        }
+
+        [TestMethod]
+        public void CheckForNoninertialNoReleaseNoOrOneConditionFrom()
+        {
+            Model model = BaseWorldGenerator.GenerateWorld(true);
+            World world = new BackendLogic().CalculateWorld(model);
+            ActionAgentsPair[] program = new ActionAgentsPair[] { new ActionAgentsPair(ScenarioConsts.Move, ScenarioConsts.Tom) };
+            //always BobRaised after MOVE by Tom
+            LogicClausesFactory logicClausesFactory = new LogicClausesFactory();
+            AfterQuery query = new AfterQuery(program, new UniformAlternative(), true,
+                logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.BobRaised, FluentSign.Positive));
+            Assert.AreEqual(false, query.Evaluate(world).IsTrue);
+
+            //possibly BobRaised after MOVE by Tom
+            logicClausesFactory = new LogicClausesFactory();
+            query = new AfterQuery(program, new UniformAlternative(), false,
+                logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.BobRaised, FluentSign.Positive));
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue);
+            
+            //possibly TomRaised after MOVE by Tom from ~TomRaised
+            logicClausesFactory = new LogicClausesFactory();
+            query = new AfterQuery(program, UniformAlternative.CreateFrom(new List<int>(), new List<int>() { ScenarioConsts.TomRaised }), false,
+                logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.TomRaised, FluentSign.Positive));
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue);
+
+            //always TomRaised after MOVE by Tom from ~TomRaised
+            logicClausesFactory = new LogicClausesFactory();
+            query = new AfterQuery(program, UniformAlternative.CreateFrom(new List<int>(), new List<int>() { ScenarioConsts.TomRaised }), true,
                 logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.TomRaised, FluentSign.Positive));
             Assert.AreEqual(true, query.Evaluate(world).IsTrue);
         }
