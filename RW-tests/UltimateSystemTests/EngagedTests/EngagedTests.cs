93847122d634e0ddb69d5a33bedd661878e65bdf
deus-ex-silicium <9478653+deus-ex-silicium@users.noreply.github.com>
2017-05-27 18:44:22 +0200

    Finnished engaged tests

--- RW-tests/UltimateSystemTests/EngagedTests/EngagedTests.cs
+++ RW-tests/UltimateSystemTests/EngagedTests/EngagedTests.cs
@@ -204,6 +204,86 @@ namespace RW_tests.UltimateSystemTests.EngagedTests
             query = new EngagedQuery(program,
                 logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.HasToy, FluentSign.Negated), false, whoisEngaged);
             Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Jack possibly engaged in LEARN by Jack, Bob from ~HasToy");
+
+            //Jack engaged in LEARN by Jack, Bob from ~HasToy ^ ~Math
+            query = new EngagedQuery(program,
+                UniformConjunction.CreateFrom(new List<int>(),new List<int> {ScenarioConsts.HasToy, ScenarioConsts.Math} ),
+                true, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Jack always engaged in LEARN by Jack, Bob from ~HasToy ^ ~Math");
+            //Jack possibly in LEARN by Jack, Bob from ~HasToy ^ ~Math
+            query = new EngagedQuery(program,
+                UniformConjunction.CreateFrom(new List<int>(), new List<int> { ScenarioConsts.HasToy, ScenarioConsts.Math }),
+                false, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Jack possibly engaged in LEARN by Jack, Bob from ~HasToy ^ ~Math");
+
+        }
+
+        [TestMethod]
+        public void TomEngaged()
+        {
+            Model model = PatriciaExamSessionScenratioGenerator.GenerateWorld();
+            World world = new BackendLogic().CalculateWorld(model);
+            BitSetFactory bitSetFactory = new BitSetFactory();
+            LogicClausesFactory logicClausesFactory = new LogicClausesFactory();
+            List<ActionAgentsPair> program = new List<ActionAgentsPair>();
+            ActionAgentsPair aap;
+            aap = new ActionAgentsPair(ScenarioConsts.Drink,
+                bitSetFactory.CreateBitSetValueFrom(new List<int>() { ScenarioConsts.Tom, ScenarioConsts.Bob, ScenarioConsts.Alice }));
+            program.Add(aap);
+
+            EngagedQuery query;
+            AgentsSet whoisEngaged = new AgentsSet(bitSetFactory.CreateBitSetValueFrom(new List<int> { ScenarioConsts.Tom }));
+
+            //Tom always engaged in DRINK by Alice, Tom, Bob
+            query = new EngagedQuery(program, new UniformAlternative(), true, whoisEngaged);
+            Assert.AreEqual(false, query.Evaluate(world).IsTrue, "Tom always engaged in DRINK by Alice, Tom, Bob");
+            //Tom possibly engaged in DRINK by Alice, Tom, Bob
+            query = new EngagedQuery(program, new UniformAlternative(), false, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Tom possibly engaged in DRINK by Alice, Tom, Bob");
+
+            //Tom always engaged in (LEARN by Alice, Bob,  DRINK by Tom)
+            aap = new ActionAgentsPair(ScenarioConsts.Learn,
+                bitSetFactory.CreateBitSetValueFrom(new List<int>() { ScenarioConsts.Bob, ScenarioConsts.Alice }));
+            program.Clear();
+            program.Add(aap);
+            aap = new ActionAgentsPair(ScenarioConsts.Drink,
+                bitSetFactory.CreateBitSetValueFrom(new List<int>() { ScenarioConsts.Tom }));
+            program.Add(aap);
+
+            query = new EngagedQuery(program, new UniformAlternative(), true, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Tom always engaged in (LEARN by Alice, Bob,  DRINK by Tom)");
+            //Tom possibly engaged in (LEARN by Alice, Bob,  DRINK by Tom)
+            query = new EngagedQuery(program, new UniformAlternative(), false, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Tom possibly engaged in (LEARN by Alice, Bob,  DRINK by Tom)");
+
+            //Tom always engaged in (DRINK by Tom, LEARN by Alice)
+            aap = new ActionAgentsPair(ScenarioConsts.Drink,
+                bitSetFactory.CreateBitSetValueFrom(new List<int>() { ScenarioConsts.Tom }));
+            program.Clear();
+            program.Add(aap);
+            aap = new ActionAgentsPair(ScenarioConsts.Learn,
+                bitSetFactory.CreateBitSetValueFrom(new List<int>() { ScenarioConsts.Alice }));
+            program.Add(aap);
+
+            query = new EngagedQuery(program, new UniformAlternative(), true, whoisEngaged);
+            Assert.AreEqual(false, query.Evaluate(world).IsTrue, "Tom always engaged in (DRINK by Tom, LEARN by Alice)");
+            //Tom possibly engaged in (DRINK by Tom, LEARN by Alice)
+            query = new EngagedQuery(program, new UniformAlternative(), false, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Tom always possibly in (DRINK by Tom, LEARN by Alice)");
+
+            //Tom always engaged in DRINK by Tom from Math
+            aap = new ActionAgentsPair(ScenarioConsts.Drink,
+                bitSetFactory.CreateBitSetValueFrom(new List<int>() { ScenarioConsts.Tom }));
+            program.Clear();
+            program.Add(aap);
+
+            query = new EngagedQuery(program, 
+                logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.Math, FluentSign.Positive), true, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Tom always engaged in DRINK by Tom from Math");
+            //Tom possibly engaged in DRINK by Tom from Math
+            query = new EngagedQuery(program,
+                logicClausesFactory.CreateSingleFluentClause(ScenarioConsts.Math, FluentSign.Positive), false, whoisEngaged);
+            Assert.AreEqual(true, query.Evaluate(world).IsTrue, "Tom possibly engaged in DRINK by Tom from Math");
         }
     }
 }
